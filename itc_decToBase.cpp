ae48f1ef337bb97f090acc23495641ba1448063e
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 00:51:51 +0300

    Update itc_decToBase.cpp

--- itc_decToBase.cpp
+++ itc_decToBase.cpp
@@ -1,15 +1,25 @@
 #include "middle_str.h"
 
-string itc_decToBase(int num, int base) {
-    string chars = "0123456789ABCDEF";
-    string res = "";
-    if (num < 0) {
-        res = "-";
-        num *= (-1);
-    }
-    while (num > 0) {
-        res += chars[num % base];
-        num /= base;
-    }
-    return res;
+string itc_rev_str(string str)
+{
+	string res = "";
+	int lenght = itc_len(str);
+	for (int i = lenght - 1; i >= 0; i--)
+		res += str[i];
+	return res;
+}
+
+string itc_decToBase(int num, int base)
+{
+	string res = "";
+	string chars = "0123456789ABCDEF";
+	if (num == 0)
+		res = "0";
+	while (num != 0)
+	{
+		res += chars[num % base];
+		num /= base;
+	}
+	res = itc_rev_str(res);
+	return res;
 }

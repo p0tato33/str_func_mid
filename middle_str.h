4d651a2cc8c1e9373097791fe7e2156e608db220
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-17 22:25:33 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -10,11 +10,11 @@ unsigned char itc_toUpper(unsigned char c);     //2
 unsigned char itc_changeCase(unsigned char c);  //3
 bool itc_сompare(string s1, string s2);         //4
 int itc_countWords(string str);	                //5
-string itc_maxCharWord(string str);				//6
-char itc_sameChar(string str);					//7
+string itc_maxCharWord(string str); //6
+char itc_sameChar(string str);	//7
 bool itc_isFirstInSecond(string s1, string s2); //8
-string itc_Cezar(string str, int k);			//9
-string itc_rmFreeSpace(string str);				//10
-bool itc_isIp(string str);						//11
-string itc_DecToBin(string str);				//12
-string itc_decToBase(int num, int base);		//13
+string itc_Cezar(string str, int k); //9
+string itc_rmFreeSpace(string str);	 //10
+bool itc_isIp(string str);//11
+string itc_DecToBin(string str);//12
+string itc_decToBase(int num, int base);//13

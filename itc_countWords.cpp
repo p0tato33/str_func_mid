af6a25b0acb103f345c6dd23f0594c58d5abaeab
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 22:36:08 +0300

    Update itc_countWords.cpp

--- itc_countWords.cpp
+++ itc_countWords.cpp
@@ -1,12 +1,26 @@
 #include "middle_str.h"
+
 int itc_countWords(string str)
 {
-	long long s1 = itc_len(str);
-	long long counter = 1;
-	for (int i = 0; i < s1; i++) {
-		if (str[i] == ' ') {
-			counter++;
-		}
-	}
-	return counter;
+    if (str == "")
+        return 0;
+    str += " ";
+    bool flag = false;
+    int counter = 0;
+    int lenght = itc_len(str);
+    for (int i = 1; i < lenght; i++)
+    {
+        if (str[i] == ' ' && (str[i - 1] >= 'a' && str[i - 1] <= 'z' || str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
+        {
+            if (!flag)
+                counter++;
+            flag = false;
+        }
+        else
+        {
+            if (!(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z'))
+                flag = true;
+        }
+    }
+    return counter;
 }

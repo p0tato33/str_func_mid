6d381da0ea1dc39872a386192e0190a15307af13
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 22:48:53 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -1,21 +1,21 @@
 #include "middle_str.h"
 
-bool itc_isFirstInSecond(string s1, string s2)
-{
-    long long length = 0;
-    bool flag = false;
-    for (long long i = 0; s2[i] != '\0'; i++)
-    {
-        if (s1[length] == s2[i])
-        {
-            length++;
-        }
-        else
-        {
-            flag = (length == itc_len(s1));
-            length = 0;
-        }
+string itc_slice_str(string str, int start, int end) {
+	long long len = itc_len(str);
+	if (start > end)
+		return str;
+	if (end >= len){ 
+		end = len - 1;
+  }string ans = "";
+	for(long long i = start; i <= end && i < len; i++) {
+		ans += str[i];
+	}
+	return ans;
+}
+
+bool itc_isFirstInSecond(string s1, string s2){
+    for(int i = 0; itc_len(itc_slice_str(s2, i, i + itc_len(s1) - 1)) == itc_len(s1); i++){
+        if(itc_slice_str(s2, i, i + itc_len(s1) - 1) == s1) return true;
     }
-    flag = (length == itc_len(s1));
-    return flag;
+    return false;
 }

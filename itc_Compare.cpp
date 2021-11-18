1864d2c8bc8f28063e0e82144004b0476f0fc510
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 21:26:46 +0300

    Update itc_Compare.cpp

--- itc_Compare.cpp
+++ itc_Compare.cpp
@@ -1,9 +1,17 @@
 #include "middle_str.h"
 
-bool itc_compare(string s1, string s2) {
-    unsigned long long size1 = itc_len(s1);
-    unsigned long long size2 = itc_len(s2);
-    if(size1 != size2 || size1 == 0 || size2 == 0)return 0;
-    for(unsigned long long i = 0;i < size1;i++)if(s1[i]!=s2[i])return 0;
-    return 1;
+bool itc_сompare(string s1, string s2) {
+    long long lenght;
+    long long lenght2;
+    lenght = itc_len(s1);
+    lenght2 = itc_len(s2);
+    long long i = 0;
+    if (lenght != lenght2)
+        return false;
+    while (s1[i] <= '\0') {
+        if (s1[i] != s2[i])
+            return false;
+        i++;
+    }
+    return true;
 }

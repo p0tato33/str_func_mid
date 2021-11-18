565f942a7bf0ae54d98286c2ea7289e59ca4dfd9
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 22:38:50 +0300

    Create slice.cpp

---
+++ slice.cpp
@@ -0,0 +1,14 @@
+#include "middle_str.h"
+
+string itc_slice_str(string str, int start, int end) {
+	long long len = itc_len(str);
+	if (start > end)
+		return str;
+	if (end >= len){ 
+		end = len - 1;
+  }string ans = "";
+	for(long long i = start; i <= end && i < len; i++) {
+		ans += str[i];
+	}
+	return ans;
+}

91086832016f2d6657161a0ff5f1f49c44ae2ad2
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 10:21:20 +0300

    Update itc_Compare.cpp

--- itc_Compare.cpp
+++ itc_Compare.cpp
@@ -7,17 +7,8 @@ long long itc_len(string str) {
     } return counter;
 }
 
-bool itc_compare(string s1, string s2)
-{
-    long long len1 = itc_len(s1);
-    long long len2 = itc_len(s2);
-    if(s1 == "" and s2 == "")
-        return 0;
-    if (len1 != len2)
-        return false;
-    for (int i = 0; i < len1; i++) {
-        if (s1[i] != s2[i])
-            return false;
-    }
+bool itc_compare(string s1, string s2) {
+    if (itc_len(s1) != itc_len(s2) || s1 == "" || s2 == "") return false;
+    for (long long i = 0; i != itc_len(s1) - 1; i++) if (s1[i] != s2[i]) return false;
     return true;
 }

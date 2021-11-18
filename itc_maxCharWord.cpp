76fc590fd3394cf825283183ef806800ab8c4ac0
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 22:24:58 +0300

    Update itc_maxCharWord.cpp

--- itc_maxCharWord.cpp
+++ itc_maxCharWord.cpp
@@ -2,34 +2,42 @@
 
 string itc_maxCharWord(string str)
 {
-	str += " ";
-	if (itc_countWords(str) == 1)
-		return "error";
-	int maxx = 0;
-	int max1 = 0;
-	int lenght = itc_len(str);
-	string str2 = "";
-	string res = "";
-	for (int i = 0; i < lenght; i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (max1 > maxx)
-			{
-				maxx = max1;
-				max1 = 0;
-				res = str2;
-			}
-			str2 = "";
-		}
-		else
-		{
-			if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
-			{
-				max1++;
-				str2 += str[i];
-			}
-		}
-	}
-	return res;
+    int i = 0;
+    int counter = 0;
+    string word;
+    string ans;
+    string str1 = "";
+    bool flag = true;
+    str += ' ';
+    while (str[i] != '\0') {
+        if (str[i] == ' ')
+            counter++;
+        i++;
+    }
+    i = 0;
+    if (counter < 2)
+        return "error";
+    while (str[i] != '\0') {
+        if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) && flag == true) {
+            str1 += str[i];
+        }
+        else if (str[i] == ' ') {
+            if (flag == true) {
+                word = str1;
+                str1 = "";
+            }
+            else
+                flag = true;
+        }
+        else {
+            str1 = "";
+            flag = false;
+        }
+        if (itc_len(word) > itc_len(ans))
+            ans = word;
+        i++;
+    }
+    if (ans == "")
+        return "error";
+    return ans;
 }

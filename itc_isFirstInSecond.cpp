471ec4992a7da4f93c9d3a774490f9a3ef260e8d
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-18 23:00:27 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -1,6 +1,6 @@
 #include "middle_str.h"
 
-string itc_slice_str(string str, int start, int end) {
+/*string itc_slice_str(string str, int start, int end) {
 	long long len = itc_len(str);
 	if (start > end)
 		return str;
@@ -11,11 +11,32 @@ string itc_slice_str(string str, int start, int end) {
 		ans += str[i];
 	}
 	return ans;
-}
+*/
 
-bool itc_isFirstInSecond(string s1, string s2){
-    for(int i = 0; itc_len(itc_slice_str(s2, i, i + itc_len(s1) - 1)) == itc_len(s1); i++){
-        if(itc_slice_str(s2, i, i + itc_len(s1) - 1) == s1) return true;
+bool itc_isFirstInSecond(string s1, string s2)
+{
+	if (itc_find_str(s2, s1) == -1)
+		return false;
+	return true;
+}
+int itc_find_str(string str1, string str2)
+{
+    int nn = 0;
+    int sravnenie = 0;
+    int newn = 0;
+    for (int i = 0; i < itc_len(str1); i++) {
+        if (str1[i] == str2[nn]) {
+            if (nn == 0)
+                newn = i;
+            nn++;
+            sravnenie++;
+            if (sravnenie == itc_len(str2))
+                return newn;
+        }
+        else {
+            nn = 0;
+            sravnenie = 0;
+        }
     }
-    return false;
+    return -1;
 }

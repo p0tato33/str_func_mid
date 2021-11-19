eafe86d32f983bceb7631860a618b64ffc5bdb36
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-19 22:35:06 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -1,41 +1,8 @@
 #include "middle_str.h"
 
-bool itc_isFirstInSecond(string s1, string s2) {
-    int lenght1 = itc_len(s1);
-    int lenght2 = itc_len(s2);
-    if (s1 == "") {
-        return true;
-    }
-    for (int i = 0; i < lenght2; i++) {
-        if (lenght2 - i < lenght1) {
-            return false;
-        }
-        if (itc_slice_str(s2, i, i + (lenght1 - 1)) == s1) {
-            return true;
-        }
-    }
-    return false;
-}
-
-
-int itc_find_str(string str1, string str2)
+bool itc_isFirstInSecond(string s1, string s2)  //вообще не поянтно что за тест который рушит
 {
-    int nn = 0;
-    int sravnenie = 0;
-    int newn = 0;
-    for (int i = 0; i < itc_len(str1); i++) {
-        if (str1[i] == str2[nn]) {
-            if (nn == 0)
-                newn = i;
-            nn++;
-            sravnenie++;
-            if (sravnenie == itc_len(str2))
-                return newn;
-        }
-        else {
-            nn = 0;
-            sravnenie = 0;
-        }
-    }
-    return -1;
+    if (s1 == "") return true;
+    if (s2 == "") return false;
+    return (itc_find_str(s2, s1) != -1);
 }

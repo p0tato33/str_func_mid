f3564e956d55161163caca9283b1c7c5857b9f23
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2021-11-19 20:41:27 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -2,10 +2,11 @@
 
 bool itc_isFirstInSecond(string s1, string s2)
 {
-	if (itc_find_str(s2, s1) == -1)
-		return false;
-	return true;
+	if (s1 == "") return true;
+	if (s2 == "") return false;
+	return (itc_find_str(s2, s1) != -1);
 }
+
 int itc_find_str(string str1, string str2)
 {
     int nn = 0;
